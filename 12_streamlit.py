c2fe75ea0d58d39030d5782164905605e518c2f0
Renjith Maniyanipurathu <renjithm1993@gmail.com>
2022-11-10 19:42:47 +0000

    final

--- 12_streamlit.py
+++ 12_streamlit.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn import linear_model
 
-data = pd.read_csv("followers_top_final.csv")
+data = pd.read_csv("followers_all_final.csv")
 
 # creating the logistic regression model
-X = data[['engagement_activity','followers', 'following', 'total_posts', 'likes_per_post', 'comments_per_post']]
-y = data['channel_associated_on_bio']
+X = data[['followers', 'following', 'total_posts', 'time_on_app_years', 'likes_per_post', 'comments_per_post', 'engagement_activity']]
+y = data['engagement_type']
 
 #  create the model 
 logistic = linear_model.LogisticRegression()
@@ -15,31 +15,33 @@ logistic = linear_model.LogisticRegression()
 logistic.fit(X,y)
 
 st.title('This is our main title')
-st.text('This is any text')
+st.text('This is any text, describing our app')
+
+# inputs
+input_feature1 = int(st.number_input('How many followers do you have?')) # featureX1
+input_feature2 = int(st.number_input('How many accounts are you following?')) # featureX2
+input_feature3 = int(st.number_input('How many total posts do you have?')) # featureX3
+input_feature4 = int(st.number_input('For how many years do you have your Instagram profile?')) # featureX4
+input_feature5 = int(st.number_input('How many likes do you have on your last 3 posts?')) # featureX5
+input_feature6 = int(st.number_input('How many comments do you have on your last 3 posts?')) # featureX6
 
 
-input_feature = st.text_input('How many followers do you have?') # featureX1
-# input_feature = st.text_input('How many accounts are you following?') # featureX2
-# input_feature = st.text_input('How many total posts do you have?') # featureX3
-# input_feature = st.text_input('How many followers do you have?') # featureX4
-# input_feature = st.text_input('How many followers do you have?') # featureX5
-# input_feature = st.text_input('How many followers do you have?') # featureX6
-# many inputs
+button = st.button('Start prediction')
 
-button = st.button('predict something')
-
-user_input = {'feat1':[input_feature], 'feat2': [input_feature]}
+user_input = {'followers':[input_feature1], 'following': [input_feature2], 'total_posts': [input_feature3], 'time_on_app_years': [input_feature4], 'likes_per_post': [input_feature5], 'comments_per_post': [input_feature6]}
 to_predict = pd.DataFrame(user_input)
+to_predict['engagement_activity'] = ((input_feature5 + input_feature6) * 100 / input_feature1)
+
 
 if button:
     # prediction model
     try:
         result = logistic.predict(to_predict)
-        if result == 0:
+        if result == "Low Engagement":
             st.write('Your engagement will be low')
         else:
             st.write('Your engagement will be high')
     except:
         st.write('Please input an integer')
 
-print(input_feature)
+print(input_feature1)
